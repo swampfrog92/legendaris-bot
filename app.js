bcededc81126e4bb0da3dd4a29f15099e452bb1a
swampfrog92 <loganholland1992@gmail.com>
2026-07-27 21:22:37 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,182 @@
+import 'dotenv/config';
+import express from 'express';
+import {
+  ButtonStyleTypes,
+  InteractionResponseFlags,
+  InteractionResponseType,
+  InteractionType,
+  MessageComponentTypes,
+  verifyKeyMiddleware,
+} from 'discord-interactions';
+import { getRandomEmoji, DiscordRequest } from './utils.js';
+import { Client, GatewayIntentBits } from 'discord.js';
+import { Pool } from 'pg';
+
+// Create an express app
+const app = express();
+// Get port, or default to 3000
+
+/*
+const pool = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+});
+*/ 
+
+const PORT = process.env.PORT || 3000
+
+  const client = new Client({
+  intents: [GatewayIntentBits.Guilds],
+  });
+
+  client.login(process.env.DISCORD_TOKEN);
+
+  client.once('ready', () => {
+  console.log('Logged in: ${client.user.tag}');
+});
+
+client.on('error', (err) => console.error('Client error:', err));
+client.on('shardError', (err) => console.error('Shard error:', err));
+
+client.login(process.env.DISCORD_TOKEN)
+  .then(() => console.log('login() resolved'))
+  .catch((err) => console.error('Login failed: ', err));
+
+app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async function (req, res) {
+  // Interaction id, type and data
+
+  const { id, type, data } = req.body;
+
+  if (type === InteractionType.PING) {
+    return res.send({ type: InteractionResponseType.PONG });
+  }
+
+  if (type === InteractionType.APPLICATION_COMMAND) {
+    const { name } = data;
+    const userId = req.body.member?.user?.id ?? req.body.user?.id;
+
+    if (name === 'help') {
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+        flags: InteractionResponseFlags.IS_COMPONENTS_V2 | InteractionResponseFlags.EPHEMERAL,
+          components: [
+            {
+              type: MessageComponentTypes.TEXT_DISPLAY,
+              content: `This will be a message explaining the instructions on how to use this bot.`
+            }
+          ]
+        },
+      });
+    }
+        else if (name === 'info') {
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+        flags: InteractionResponseFlags.IS_COMPONENTS_V2 | InteractionResponseFlags.EPHEMERAL,
+          components: [
+            {
+              type: MessageComponentTypes.TEXT_DISPLAY,
+              content: `This will be a message explaining the information regarding the league status`
+            }
+          ]
+        },
+      });
+    }
+else if (name === 'rank') {
+  return res.send({
+    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+    data: {
+      flags: InteractionResponseFlags.IS_COMPONENTS_V2 | InteractionResponseFlags.EPHEMERAL,
+      components: [
+        {
+          type: MessageComponentTypes.TEXT_DISPLAY,
+          content: `This will display the user's rank for: ${userId}`
+        }
+      ]
+    },
+  });
+}
+
+else if (name === 'notify') {
+
+  try {
+
+    const user = await client.users.fetch(userId, {force:true});
+    await user.send('This is a DM from the bot!');
+
+    return res.send({
+      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+      flags: InteractionResponseFlags.IS_COMPONENTS_V2 | InteractionResponseFlags.EPHEMERAL,
+      data: {
+        content: 'Check your DMs!',
+      },
+    });
+  } catch (err) {
+    console.error('Failed to send DM:', err);
+    return res.send({
+      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+      data: {
+        flags: InteractionResponseFlags.EPHEMERAL,
+        content: "I couldn't DM you — you might have DMs disabled for this server.",
+      },
+    });
+  }
+}
+
+else if (name === 'results') {
+  const oppUserId = req.body.data.options?.find(opt => opt.name === 'opp')?.value;
+  const yourVP = req.body.data.options?.find(opt => opt.name === 'playervp')?.value;
+  const oppVP = req.body.data.options?.find(opt => opt.name === 'oppvp')?.value;
+  const victoryMessage = yourVP > oppVP ? userId : yourVP < oppVP ? oppUserId : `It's a tie!`;
+
+  try{
+    /*
+    await pool.query(
+      'INSERT INTO games (player_a, player_b, player_a_points, player_b_points) VALUES ($1, $2, $3, $4)',
+      [userId, oppUserId, yourVP, oppVP]
+    ); 
+    */
+    return res.send({
+      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+      data: {
+        flags: InteractionResponseFlags.IS_COMPONENTS_V2,
+        components: [
+          {
+            type: MessageComponentTypes.TEXT_DISPLAY,
+            content: `This will display the result: <@${userId}> - ${yourVP} --- <@${oppUserId}> - ${oppVP}. The winner is: <@${victoryMessage}>`
+          }
+        ]
+      },
+    });
+  } catch (err){
+    console.error('Database error while submitting results: ', err);
+    return res.send({
+      type: InteractionResponseFlags.CHANNEL_MESSAGE_WITH_SOURCE,
+      data: {
+        flags: InteractionResponseFlags.IS_COMPONENTS_V2,
+        components: [
+          {
+            type: MessageComponentTypes.TEXT_DISPLAY,
+            content: 'Error while submitting your results. Please try again later. Contact support team if problem persists.'
+          }
+        ]
+      }
+    })
+  }
+}
+
+    console.error(`unknown command: ${name}`);
+    return res.status(400).json({ error: 'unknown command' });
+  }
+
+  console.error('unknown interaction type', type);
+  return res.status(400).json({ error: 'unknown interaction type' });
+});
+
+app.listen(PORT, () => {
+  console.log('Listening on port', PORT);
+});
---
+++ commands.js
@@ -0,0 +1,66 @@
+import 'dotenv/config';
+import { capitalize, InstallGlobalCommands } from './utils.js';
+
+const HELP_COMMAND = {
+  name: 'help',
+  description: 'Receive an ephemeral message explaining how to use this bot',
+  type: 1,
+  integration_types: [0, 1],
+  contexts: [0, 1, 2],
+};
+
+const INFO_COMMAND = {
+  name: 'info',
+  description: 'Receive an ephemeral message about the current league and its status',
+  type: 1,
+  integration_types: [0, 1],
+  contexts: [0, 1, 2],
+};
+
+const RANK_COMMAND = {
+  name: 'rank',
+  description: 'Receive an ephemeral message about your current rank and Elo',
+  type: 1,
+  integration_types: [0,1],
+  contexts: [0,1,2],
+};
+
+const NOTIFY_COMMAND = {
+  name: 'notify',
+  description: 'send a dm',
+  type: 1,
+  integration_types: [0,1],
+  contexts: [0,1,2]
+};
+
+const RESULTS_COMMAND = {
+  name: 'results',
+  description: 'Submit and publish the results of a recent game',
+  type: 1,
+  integration_types: [0,1],
+  contexts: [0,1,2],
+  options: [
+    {
+      type: 6,
+      name: 'opp',
+      description: 'Your opponent in the game',
+      required: true
+    },
+    {
+      type: 4,
+      name: 'playervp',
+      description: 'Your victory points',
+      required: true
+    },
+    {
+      type: 4,
+      name: 'oppvp',
+      description: 'Your opponents victory points',
+      required: true
+    }
+  ]
+};
+
+const ALL_COMMANDS = [HELP_COMMAND, INFO_COMMAND, RANK_COMMAND, NOTIFY_COMMAND, RESULTS_COMMAND];
+
+InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
