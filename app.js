e15c68d0d3bb48d5f241a2055f40da0ef5aa7907
Logan <loganholland1992@gmail.com>
2026-07-28 15:43:22 -0400

    Creating match submission

--- app.js
+++ app.js
@@ -128,6 +128,7 @@ else if (name === 'results') {
   try{
     await prisma.match.create({
       data: {
+        gameSystemId: 1,
         submittedById: userId,
         playerOneId: userId,
         playerTwoId: oppUserId,
