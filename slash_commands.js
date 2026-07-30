4023644c1ac71f4ce28b50db27e2579d4dd26036
Logan <loganholland1992@gmail.com>
2026-07-29 21:20:19 -0400

    adding rank command

--- slash_commands.js
+++ slash_commands.js
@@ -16,7 +16,7 @@ export const prisma = new PrismaClient();
 export async function rank_request(res, req) {
 
     try{
-        const userId = await prisma.user.findUnique({ where: { discordId: req.body.member?.user?.id ?? req.body.user?.id }, select: { id: true } });
+        const userId = await prisma.user.findUnique({ where: { discordId: req.body.member?.user?.id ?? req.body.user?.id }, select: { id: true } })?.id;
         let community = await prisma.community.findUnique({
             where: {
                 id: 'cms6g007x0001lo0psadsm3w7',
