4077c9f6e358387ffd1810b6fc879f20fa5e89e6
Logan <loganholland1992@gmail.com>
2026-07-30 20:13:18 -0400

    adding DiscordChapter table

---
+++ chapter.js
@@ -0,0 +1,28 @@
+import {
+  ButtonStyleTypes,
+  InteractionResponseFlags,
+  InteractionResponseType,
+  InteractionType,
+  MessageComponentTypes,
+  verifyKeyMiddleware,
+} from 'discord-interactions';
+
+import { PrismaClient } from "./generated/prisma/client.js";
+
+export async function getChapter(res, req, prisma){
+    try{
+        const discordId = req.body.guild_id;
+        const communityId = (await prisma.discordChapter.findUnique({
+            where:{
+                id: discordId
+            },
+            select: {
+                communityId: true
+            }
+        }))?.communityId;
+
+        return communityId;
+    } catch(err){
+        console.error("Error while retrieving CommunityId", error);
+    }
+}
