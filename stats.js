4b99b03178702ddfd7ded86d2d56fd9a79084a37
Logan <loganholland1992@gmail.com>
2026-08-08 10:09:06 -0400

    Adding the /stats command

--- stats.js
+++ stats.js
@@ -16,9 +16,9 @@ export async function getFactionStats(res, req, prisma){
         console.log(communityId);
         const userId = (await prisma.user.findUnique({ where: { discordId: req.body.member?.user?.id ?? req.body.user?.id }, select: { id: true } }))?.id;
         console.log(userId);
-        const communityMemberId = (await prisma.communityMember.findUnique({ where: { communityId_userId: { communityId, userId } }, select: { id: true } }));
-        console.log(communityMemberId);
-        const factionStats = (await prisma.playerFaction.findMany({ where: { communityMemberId: communityMemberId.id } }));
+        const communityMember = (await prisma.communityMember.findUnique({ where: { userId_communityId: { userId, communityId } }, select: { id: true } }));
+        console.log(communityMember);
+        const factionStats = (await prisma.playerFaction.findMany({ where: { communityMemberId: communityMemberx.id } }));
         console.log(factionStats);
 
         let msg = "Your faction stats: \n\n";
@@ -30,6 +30,6 @@ export async function getFactionStats(res, req, prisma){
         return msg;
 
     } catch(err){
-        console.error("Error while retrieving Faction Stats", error);
+        console.error("Error while retrieving Faction Stats", err);
     }
 }
