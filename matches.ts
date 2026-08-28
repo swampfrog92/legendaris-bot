9de301469f52bf17a2cf4f0f4068e958395af8c5
Logan <loganholland1992@gmail.com>
2026-08-28 12:34:13 -0400

    merging prisma schemas

--- matches.ts
+++ matches.ts
@@ -8,6 +8,7 @@ import {
 } from 'discord-interactions';
 
 import { PrismaClient } from "./generated/prisma/client.js";
+import { findUserIdFromDiscordId } from "./search.js";
 
 export async function findRecentMatchesForDisplay(prisma: PrismaClient, userId: string, numMatches: number): Promise<string> {
     try{
@@ -42,4 +43,54 @@ export async function findRecentMatchesForDisplay(prisma: PrismaClient, userId: 
         return "Error while finding recent match history. Please contact our support team if problem persists";
     }
 
+}
+
+export async function setMatchAsDisputed(prisma: PrismaClient, matchId: string, note: string): Promise<boolean> {
+    try{
+        await prisma.match.update({
+            where: {
+                id: matchId
+            },
+            data: {
+                status: "DISPUTED",
+                disputeReason: note
+            }
+        });
+        return true;
+    } catch(err){
+        console.log("Error while setting match as disputed", err);
+        return false;
+    }
+}
+
+export async function findRecentMatchesForAutocomplete(req: any, res: any, prisma: PrismaClient) {
+
+    const focused = req.body.data.options.find((option: { name: string; value: string; focused?: boolean }) => option.focused);
+    const userId = await findUserIdFromDiscordId(prisma, req.body.member?.user?.id ?? req.body.user?.id);
+
+    const matches = await prisma.match.findMany({
+        where: {
+            OR: [
+                {playerOneId: userId},
+                {playerTwoId: userId}
+            ]
+        },
+        select: {
+            playerOneId: true,
+            playerTwoId: true,
+            createdAt: true,
+            id: true
+        },
+        take: 25
+    });
+
+    return res.send({
+        type: 8,
+        data: {
+            choices: matches.map(match => ({
+                name: `<@${match.playerOneId}> vs <@${match.playerTwoId}> on ${match.createdAt.toLocaleDateString()}`,
+                value: match.id
+            }))
+        }
+    });
 }
