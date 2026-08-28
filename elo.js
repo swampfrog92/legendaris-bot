afde8890581dd5378bc7d6950d20f6c05d05028b
Logan <loganholland1992@gmail.com>
2026-08-27 20:09:16 -0400

    adding faction elo to matchcommunity submissions.

--- elo.js
+++ elo.js
@@ -130,14 +130,15 @@ export async function updateFactionElo(prisma, playerOne, playerTwo, playerVP, o
                     increment: playerTwoMatchDelta.drawsDelta
                 }
             }
-        });    
+        });
+        return {playerOneFactionEloBefore: playerOneFaction.lifetimeElo, playerTwoFactionEloBefore: playerTwoFaction.lifetimeElo, playerOneFactionEloAfter: playerOneNewFactionElo, playerTwoFactionEloAfter: playerTwoNewFactionElo};    
     } catch(err){
         console.error('Error while updating faction Elo', err);
     }
 }
 
 // Updates the community elo after a match. Takes five arguments: the prisma client, playerOne and playerTwo (both rows from communityMember), and the victory points for each player. Updates the lifetimeElo for both players in the community, and creates a new matchCommunity entry.
-export async function updateElo(prisma, playerOne, playerTwo, playerVP, oppVP, matchId, req){
+export async function updateElo(prisma, playerOne, playerTwo, playerVP, oppVP, matchId, factionEloUpdate, req){
     try{
         // Calculates the new lifetime Elo for the players.
         const playerOneNewElo = playerVP > oppVP ? adjustEloForVictory(playerOne.lifetimeElo, playerTwo.lifetimeElo) : playerVP < oppVP ? adjustEloForDefeat(playerOne.lifetimeElo, playerTwo.lifetimeElo) : adjustEloForTie(playerOne.lifetimeElo, playerTwo.lifetimeElo);
@@ -164,6 +165,10 @@ export async function updateElo(prisma, playerOne, playerTwo, playerVP, oppVP, m
                 playerTwoSeasonEloBefore: playerTwo.seasonElo,
                 playerOneSeasonEloAfter: playerOneNewSeasonElo,
                 playerTwoSeasonEloAfter: playerTwoNewSeasonElo,
+                playerOneFactionEloBefore: factionEloUpdate.playerOneFactionEloBefore,
+                playerTwoFactionEloBefore: factionEloUpdate.playerTwoFactionEloBefore,
+                playerOneFactionEloAfter: factionEloUpdate.playerOneFactionEloAfter,
+                playerTwoFactionEloAfter: factionEloUpdate.playerTwoFactionEloAfter,
                 seasonId: activeSeason.id,
             }
         });
@@ -280,10 +285,10 @@ export async function createMatch(res, req, prisma){
         const playerTwo = (await prisma.communityMember.findFirst({where: {userId: oppUserDbId, communityId}}));
 
         // Updates the specific Elo for the faction played. 
-        updateFactionElo(prisma, playerOne, playerTwo, yourVP, oppVP, yourFaction, oppFaction);
+        const factionEloUpdate = updateFactionElo(prisma, playerOne, playerTwo, yourVP, oppVP, yourFaction, oppFaction);
 
         // Updates the community Elo for the players.
-        updateElo(prisma, playerOne, playerTwo, yourVP, oppVP, matchId, req);
+        updateElo(prisma, playerOne, playerTwo, yourVP, oppVP, matchId, factionEloUpdate, req);
     } catch(err){
         console.error('Error while creating match', err);
     }
